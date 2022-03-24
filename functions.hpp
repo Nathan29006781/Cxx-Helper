df5ea9709aa0cee6c67eb35fca0f946e3ae5db6b
Nathan <nathan29006781@hotmail.com>
2022-03-23 23:18:54 -0400

    Kinda working binOp

--- functions.hpp
+++ functions.hpp
@@ -4,13 +4,39 @@
 //maybe equation support here? if not, then math.hpp
 
 #include <functional>
+#include <unordered_map>
+#include <string>
 
+//create arithmetic constraint
+template <typename T>
 struct BinArthmOp{
-  std::function<long double()> first_arg;
-  std::function<long double()> second_arg;
-  std::function<long double(long double, long double)> operation;
+  // static std::unordered_map<std::string, std> operators {
+  //   "+", "-", "*", "x", "/", "^", "**", "&&", "&", "|", "||", ">", 
+  
+  // };
 
+  std::function<T()> first_arg;
+  std::function<T()> second_arg;
+  std::function<T(T, T)> operation;
 
-  //() operator
+  std::function<T()> result;
+
+  BinArthmOp(std::string equation){
+    result = std::function([this](){return this->operation(this->first_arg(), this->second_arg());});
+  }
+
+  // BinArthmOp(std::function<T(T, T)> operation, std::function<T()> first_arg, std::function<T()> second_arg):
+  // operation(operation), first_arg(first_arg), second_arg(second_arg){
+  //   result = std::function([this](){return this->operation(this->first_arg(), this->second_arg());});
+  // }
+
+  BinArthmOp(std::function<T(const T&, const T&)> operation, const T& first_arg, const T& second_arg):
+  operation(operation), first_arg([&first_arg](){return first_arg;}), second_arg([&second_arg](){return second_arg;}){
+    result = std::function([this](){return this->operation(this->first_arg(), this->second_arg());});
+  }
+
+  // ctor with string with op and placeholders __1 and __2, provide
+
+  T operator ()(){return result();}
   //check if value-convertible by looking at operands. convert if convertible
 };
--- main.cpp
+++ main.cpp
@@ -1,11 +1,15 @@
 #include <ios>
 #include <iostream>
 #include "math.hpp"
+#include "functions.hpp"
 #include <cmath>
 
 //gitignore executables, .bin, and main.cpp
 
 int main(){
-  std::cout << std::boolalpha << in_range(0.0, -5.4, 4.25) << std::endl;
+  BinArthmOp<int> op (std::multiplies<int>(), 10, 2);
+
+  // std::function<int(int, int)> t = std::plus<int>();
+  std::cout << std::boolalpha << op() << std::endl;
   return 0;
 }
